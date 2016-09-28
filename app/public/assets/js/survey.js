1e4b49c357275197f7a583b9bff0d54cb02f4cf8
rgtalbot <rgtalbot@users.noreply.github.com>
2016-09-28 14:18:57 -0400

    served up static files

--- app/public/assets/js/survey.js
+++ app/public/assets/js/survey.js
@@ -1,16 +1,36 @@
-$("#submit").on('click', function() {
-   console.log("working");
-    var userData = {
-        name: $("#name").val().trim(),
-        photo: $("#photo").val().trim(),
-        scores: [$("#q1").val(), $("#q2").val(), $("#q3").val(), $("#q4").val(), $("#q5").val(), $("#q6").val(), $("#q7").val(), $("#q8").val(), $("#q9").val(), $("#q10").val()]
-    };
+$("#submit").on('click', function () {
 
-    var currentURL = window.location.origin;
+    function validate() {
+        var isValid = true;
+        $('.form-control').each(function () {
+            if ($(this).val() == '') {
+                isValid = false;
+            }
+        });
+        $('.chosen-select').each(function () {
+            if ($(this).val() == "") {
+                isValid = false;
+            }
+        });
+        return isValid;
+    }
 
-    //AJAX post to API
-    $.post(currentURL + "/api/frineds", userData, function(data) {
+    if (validate() == true) {
+        var userData = {
+            name: $("#name").val().trim(),
+            photo: $("#photo").val().trim(),
+            scores: [$("#q1").val(), $("#q2").val(), $("#q3").val(), $("#q4").val(), $("#q5").val(), $("#q6").val(), $("#q7").val(), $("#q8").val(), $("#q9").val(), $("#q10").val()]
+        };
 
-    })
+        var currentURL = window.location.origin;
 
+        //AJAX post to API
+        $.post(currentURL + "/api/frineds", userData, function (data) {
+
+        })
+    } else {
+        alert("Please fill out form entirely");
+    }
+
+    return false
 });
--- server.js
+++ server.js
@@ -17,6 +17,8 @@ app.use(bodyParser.json({type:'application/vnd.api+json'}));
 require('./app/routing/api-routes.js')(app);
 require('./app/routing/html-routes.js')(app);
 
+app.use(express.static('public'));
+
 //LISTENER
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
